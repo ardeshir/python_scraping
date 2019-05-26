5beb3182a9f93fd200fdae7a3cbea6087cef2524
ardeshir sepahsalar <ardeshir.org@gmail.com>
2019-05-26 17:06:35 -0500

    adding beautiful soup

--- scrapetest.py
+++ scrapetest.py
@@ -3,9 +3,13 @@
 """  Scraping Websites with Python3 """
 
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
 html = urlopen("http://pythonscraping.com/pages/page1.html")
 
 print("Python3 Scraping")
 print(" ") 
 
-print( html.read() ) 
+bs = BeautifulSoup(html.read(), 'html.parser')
+
+print(bs.h1 ) 
